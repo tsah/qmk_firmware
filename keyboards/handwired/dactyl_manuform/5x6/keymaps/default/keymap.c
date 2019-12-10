e03337d196313cb69aeca4e8dc1d8e69d27a76c6
tsah <tsah.weiss@gmail.com>
2019-12-10 09:11:18 +0200

    added number and function layer. swapped backspace and delete, pgup and pgdown. Added standalone shift to the number layer to use in intellij

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -26,8 +26,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_PGDN, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
                       KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
                                       RAISE,KC_SPC,                        KC_ENT, LOWER,
-                                      KC_BSPC,  KC_DEL,                    KC_HOME,KC_END,                         
-                                      KC_ESC, KC_GRV,                        KC_PGDN, KC_PGUP
+                                      KC_DEL,  KC_BSPC,                    KC_HOME,KC_END,                         
+                                      KC_ESC, KC_GRV,                        KC_PGUP, KC_PGDOWN
   ),
 
   [_LOWER] = LAYOUT_5x6(
@@ -43,11 +43,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT_5x6(
        KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-       _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
-       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
-       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
-                                               _______,_______,            KC_EQL ,_______,
-                                               _______,_______,            _______,_______,
+       _______,KC_F1, KC_F2, KC_F3, KC_F4,     KC_LBRC,                        KC_RBRC,KC_1,KC_2,KC_3 ,KC_MINS,KC_MUTE,
+       _______,KC_F5,KC_F6  ,KC_F7,KC_F8,KC_LPRN,                        KC_RPRN,KC_4,KC_5,KC_6,KC_PLUS,KC_VOLU,
+       _______,KC_F9,KC_F10,KC_F11,KC_F12,KC_LCBR,                        KC_RCBR,KC_7,KC_8,KC_9,KC_0,KC_VOLD,
+                                               _______,_______,            KC_ASTR ,KC_SLSH,
+                                               _______,_______,            KC_EQL,KC_RSFT,
                                                _______,_______,            _______,_______,
                                                _______,_______,            _______,_______
   ),
