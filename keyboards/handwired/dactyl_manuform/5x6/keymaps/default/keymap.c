21cac9cd5f110d0357820c1a047f46c1df2dd80a
tsah <tsah.weiss@gmail.com>
2019-12-11 09:49:31 +0200

    Added minus to thumb cluster

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
                                       RAISE,KC_SPC,                        KC_ENT, LOWER,
                                       KC_DEL,  KC_BSPC,                    KC_TAB,KC_ESC,                         
-                                      KC_QUOT, KC_GRV,                        KC_EQL, KC_PLUS
+                                      KC_QUOT, KC_GRV,                        KC_EQL, KC_MINS
   ),
 
   [_LOWER] = LAYOUT_5x6(
