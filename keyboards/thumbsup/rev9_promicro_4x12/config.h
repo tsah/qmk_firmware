9915d85a1fe293fefafe8a62f1633c9773cbe5d8
tsah-alike <tsah@alike.health>
2024-07-16 09:35:36 +0300

    Update iris keymap

--- keyboards/thumbsup/rev9_promicro_4x12/config.h
+++ keyboards/thumbsup/rev9_promicro_4x12/config.h
@@ -15,11 +15,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #pragma once
 
-
-//Cirque ProGlide Touchpad configuration
+// Cirque ProGlide Touchpad configuration
 #define POINTING_DEVICE_ROTATION_90
 #define CIRQUE_PINNACLE_TAP_ENABLE
 #define CIRQUE_PINNACLE_SECONDARY_TAP_ENABLE
