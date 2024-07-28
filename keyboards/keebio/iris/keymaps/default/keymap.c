020baeb329d87b77c8d608c2a4f57baa73c3e02e
tsah-alike <tsah@alike.health>
2024-07-28 10:18:01 +0300

    WIP 34

--- keyboards/keebio/iris/keymaps/default/keymap.c
+++ keyboards/keebio/iris/keymaps/default/keymap.c
@@ -2,66 +2,93 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "print.h"
 
 #define _BASE 0
-#define _COLEMAK 1
+#define _ALT_LAYOUT 1
 #define _NUMBERS 2
 #define _NAV 3
-#define _SYMBOLS 4
+#define _MODS 4
 
 
 enum custom_keycodes {
     NUMBERS = SAFE_RANGE,
     NAV,
     SYMBOLS,
-    MACRO_LAYER_TOGGLE_LAYOUT,
-    MACRO_LAYER_TOGGLE_LAYOUT_LANGUAGE
+    M_LAYER_TOGGLE_LAYOUT,
+    M_LAYER_TOGGLE_LAYOUT_LANGUAGE,
+    M_UP_DIR,
+    M_THE,
+    M_MENT,
+    M_UEN,
+    M_ENT,
+    M_ION,
+    M_ON
 };
 
-enum combo_events {
-    COMBO_MACRO_LAYER_TOGGLE_LAYOUT_LANGUAGE,
-    COMBO_MACRO_LAYER_TOGGLE_LAYOUT,
-    COMBO_LENGTH  // This represents the number of combos
+// Combos
+// Right mods
+const uint16_t PROGMEM c_jgui[] = {MO(_NAV), KC_J, COMBO_END};
+const uint16_t PROGMEM c_kalt[] = {MO(_NAV), KC_K, COMBO_END};
+const uint16_t PROGMEM c_lctrl[] = {MO(_NAV), KC_L, COMBO_END};
+const uint16_t PROGMEM c_clnsft[] = {MO(_NAV), KC_SCLN, COMBO_END};
+
+// Left mods
+const uint16_t PROGMEM c_fgui[] = {MO(_NUMBERS), KC_F, COMBO_END};
+const uint16_t PROGMEM c_dalt[] = {MO(_NUMBERS), KC_D, COMBO_END};
+const uint16_t PROGMEM c_sctrl[] = {MO(_NUMBERS), KC_S, COMBO_END};
+const uint16_t PROGMEM c_asft[] = {MO(_NUMBERS), KC_A, COMBO_END};
+
+// home row combos
+
+const uint16_t PROGMEM c_jk[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM c_df[] = {KC_D, KC_F, COMBO_END};
+
+combo_t key_combos[] = {
+    COMBO(c_jgui, OSM(MOD_RGUI)),
+    COMBO(c_kalt, OSM(MOD_RALT)),
+    COMBO(c_lctrl, OSM(MOD_RCTL)),
+    COMBO(c_clnsft, OSM(MOD_RSFT)),
+
+    COMBO(c_fgui, OSM(MOD_LGUI)),
+    COMBO(c_dalt, OSM(MOD_LALT)),
+    COMBO(c_sctrl, OSM(MOD_LCTL)),
+    COMBO(c_asft, OSM(MOD_LSFT)),
+
+    COMBO(c_jk, KC_ENT),
+    COMBO(c_df, KC_SPC),
+
 };
 
-const uint16_t PROGMEM combo_macro_layer_toggle_layout_language[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM combo_macro_layer_toggle_layout[] = {KC_Z, KC_X, COMBO_END};
-
-// Create an array of combos
-combo_t key_combos[COMBO_LENGTH] = {
-    [COMBO_MACRO_LAYER_TOGGLE_LAYOUT_LANGUAGE] = COMBO(combo_macro_layer_toggle_layout_language, MACRO_LAYER_TOGGLE_LAYOUT_LANGUAGE),
-    [COMBO_MACRO_LAYER_TOGGLE_LAYOUT] = COMBO(combo_macro_layer_toggle_layout, MACRO_LAYER_TOGGLE_LAYOUT),
-};
-
-static bool in_colemak = false;
+static bool in_alt_layout = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______,  _______,    _______,    _______,    _______,    _______,                               _______,    _______,    _______,    _______,    _______,    _______,
+     _______,  _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, LSFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G,          KC_H,    RGUI_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L), RSFT_T(KC_SCLN), _______,
+     _______,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,          _______,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
+ _______, LSFT_T(KC_Z), LCTL_T(KC_X), LALT_T(KC_C), LGUI_T(KC_V), KC_B, _______,     _______, KC_N, RGUI_T(KC_M), RALT_T(KC_COMM), RCTL_T(KC_DOT), RSFT_T(KC_SLSH), _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                     _______, MO(_NUMBERS), KC_SPC,             KC_ENT,  MO(_NAV),   _______
+                                     _______, MO(_NUMBERS), LT(_MODS, KC_SPC),   LT(_MODS, KC_ENT),  MO(_NAV),   _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_COLEMAK] = LAYOUT(
+  [_ALT_LAYOUT] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______,  _______, _______, _______, _______, _______,                               _______,    _______,    _______,    _______,    _______,    _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                               KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    _______,
+     _______,  KC_V,    KC_M,    KC_L,    KC_C,    KC_P,                               KC_X,    KC_F,    KC_O,    KC_U,    KC_J,    _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, LSFT_T(KC_A), LCTL_T(KC_R), LALT_T(KC_S), LGUI_T(KC_T), KC_G,          KC_M,    RGUI_T(KC_N), RALT_T(KC_E), RCTL_T(KC_I), RSFT_T(KC_O), _______,
+     _______, KC_S,     KC_T,    KC_R,    KC_D,    KC_Y,                               KC_DOT,  KC_N,    KC_A,    KC_E,    KC_I, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    _______,          _______,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, _______,
+  _______, LSFT_T(KC_Z), LCTL_T(KC_K), LALT_T(KC_Q), LGUI_T(KC_G), KC_W, _______,  _______,KC_B, RGUI_T(KC_H), RALT_T(KC_QUOT), RCTL_T(KC_SCLN), RSFT_T(KC_COMM), _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                     _______, NUMBERS, KC_SPC,                    KC_ENT,    NAV,   _______
+                                    _______, MO(_NUMBERS), KC_SPC,               KC_ENT, MO(_NAV), _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -69,13 +96,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, _______, _______, _______, _______,  _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, KC_BSLS, KC_LBRC,                            KC_RBRC,  KC_1,    KC_2,    KC_3,    _______,    _______,
+     _______, TO(_BASE), TO(_ALT_LAYOUT), _______, KC_BSLS, KC_LBRC,                  KC_RBRC,  KC_1,    KC_2,    KC_3,    _______,    _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______,  KC_PIPE, KC_GRV,   KC_TAB, KC_ESC,  KC_LPRN,                           KC_RPRN,  KC_4,    KC_5,    KC_6,    KC_0, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, KC_LCBR, _______,          _______, KC_RCBR,  KC_7,    KC_8,    KC_9,    _______,    _______,
+     _______, _______, _______, _______, KC_RSFT,  KC_LCBR, _______,     _______, KC_RCBR,  KC_7,    KC_8,    KC_9,    _______,    _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _______,                    KC_DEL,  KC_LSFT, _______
+                                    _______, _______, _______,                    KC_DEL,  KC_RSFT, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -83,23 +110,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, _______,  _______, _______, _______, _______,                           _______,  _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_HOME,  KC_PGDN, KC_PGUP, KC_END, KC_QUOT, _______,
+     _______, _______, _______,   _______, _______,  _______,                          KC_HOME,  KC_PGDN, KC_PGUP, KC_END, KC_QUOT, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, KC_MINS, KC_PLUS, KC_UNDS, KC_EQL, KC_UNDS,                             KC_LEFT,  KC_DOWN, KC_UP, KC_RIGHT, KC_DQT, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_MINS, KC_LPRN,          _______, KC_0,    _______,  _______, KC_DLR, _______, _______,
+     _______, _______, _______, _______, _______, _______, _______,          _______, _______,    _______,  _______, _______, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, KC_RSFT, KC_BSPC,                   _______, _______, _______
+                                    _______, KC_LSFT, KC_BSPC,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_SYMBOLS] = LAYOUT(
+  [_MODS] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, _______, _______, _______, _______,                            _______, KC_EXLM, KC_AT, KC_HASH, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_PIPE, KC_BSLS, LCTL(KC_BSLS), KC_QUOT, LSFT(KC_QUOT),                 _______, KC_DLR, KC_PERC, KC_CIRC, KC_RPRN, _______,
+     _______, OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_LGUI), LSFT(KC_QUOT),  _______, OSM(MOD_RGUI), OSM(MOD_RALT), OSM(MOD_RCTL), KC_RPRN, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, _______, _______, _______, _______, _______,          _______, _______, KC_AMPR, KC_ASTR, KC_LPRN, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
@@ -111,26 +138,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case MACRO_LAYER_TOGGLE_LAYOUT:
+    case M_LAYER_TOGGLE_LAYOUT:
       if (record->event.pressed) {
-        if (in_colemak) {
-          layer_off(_COLEMAK);
-          in_colemak = false;
+        if (in_alt_layout) {
+          layer_off(_ALT_LAYOUT);
+          in_alt_layout = false;
         } else {
-          layer_on(_COLEMAK);
-          in_colemak = true;
+          layer_on(_ALT_LAYOUT);
+          in_alt_layout = true;
         }
       }
       return false;
       break;
-    case MACRO_LAYER_TOGGLE_LAYOUT_LANGUAGE:
+    case M_LAYER_TOGGLE_LAYOUT_LANGUAGE:
       if (record->event.pressed) {
-        if (in_colemak) {
-          layer_off(_COLEMAK);
-          in_colemak = false;
+        if (in_alt_layout) {
+          layer_off(_ALT_LAYOUT);
+          in_alt_layout = false;
         } else {
-          layer_on(_COLEMAK);
-          in_colemak = true;
+          layer_on(_ALT_LAYOUT);
+          in_alt_layout = true;
         }
         register_code(KC_LCTL);
         register_code(KC_LALT);
@@ -143,6 +170,44 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case M_UP_DIR: SEND_STRING("./"); break;
+    case M_THE: SEND_STRING("the"); break;
+    case M_MENT: SEND_STRING("ment"); break;
+    case M_UEN: SEND_STRING("uen"); break;
+    case M_ENT: SEND_STRING("ent"); break;
+    case M_ION: SEND_STRING("ion"); break;
+    case M_ON: SEND_STRING("on"); break;
   }
   return true;
+}
+
+uint16_t get_alt_repeat_key_keycode_user(uint16_t keycode, uint8_t mods) {
+    /* bool shifted = (mods & MOD_MASK_SHIFT);  // Was Shift held? */
+    /* bool ctrl = (mods & MOD_MASK_CTRL); */
+    /* bool alt = (mods & MOD_MASK_ALT); */
+    /* bool GUI = (mods & MOD_MASK_GUI); */
+    print("Hello, world");
+    switch (keycode) {
+        case KC_A: return KC_O;
+        case KC_C: return KC_Y;
+        case KC_D: return KC_Y;
+        case KC_E: return KC_U;
+        case KC_G: return KC_Y;
+        case KC_I: return M_ON;
+        case KC_L: return KC_K;
+        case KC_M: return M_ENT;
+        case KC_N: return M_ION;
+        case KC_O: return KC_A;
+        case KC_P: return KC_Y;
+        case KC_Q: return M_UEN;
+        case KC_R: return KC_L;
+        case KC_S: return KC_K;
+        case KC_T: return M_MENT;
+        case KC_U: return KC_E;
+        case KC_Y: return KC_P;
+        case KC_SPC: return M_THE;
+        case KC_DOT: return M_UP_DIR;
+    }
+
+    return KC_TRNS;
 }
